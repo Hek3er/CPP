da0c3aee493ea93134a2f64bdea74d36b38d4481
Hek3er <aymen.zainabi@gmail.com>
2025-03-29 05:32:49 +0000

    ex00 finished

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -1,4 +1,7 @@
 #include "ScalarConverter.hpp"
+#include <climits>
+#include <limits>
+#include <sstream>
 
 ScalarConverter::ScalarConverter() {
 
@@ -47,10 +50,119 @@ void	handle_int( const std::string str ) {
 	PRINT_DOUBLE(static_cast<double>(out), ".0");
 }
 
+bool isNaN( double n ) {
+	return n != n;
+}
+
+bool isNaN( float n ) {
+	return n != n;
+}
+
+bool isInf( double n ) {
+	return n == std::numeric_limits<double>::infinity() || n == -std::numeric_limits<double>::infinity();
+}
+
+bool isInf( float n ) {
+	return n == std::numeric_limits<float>::infinity() || n == -std::numeric_limits<float>::infinity();
+}
+
 void	handle_double( const std::string str ) {
-	if (str == "nan") {
-		
+	std::stringstream ss(str);
+	double out;
+	ss >> out;
+	if (ss.fail()) {
+		PRINT_CHAR("impossible", "");
+		PRINT_INT("impossible", "");
+		PRINT_FLOAT("impossible", "");
+		PRINT_DOUBLE("impossible", "");
+		return ;
 	}
+	if (isNaN(out)) {
+		PRINT_CHAR("impossible", "");
+		PRINT_INT("impossible", "");
+		PRINT_FLOAT(static_cast<float>(out), "f");
+		PRINT_DOUBLE(out, "");
+		return ;
+	}
+	if (isInf(out)) {
+		PRINT_CHAR("impossible", "");
+		PRINT_INT("impossible", "");
+		PRINT_FLOAT(static_cast<float>(out), "f");
+		PRINT_DOUBLE(out, "");
+		return ;
+	}
+	if (static_cast<int>(out) >= 32 && static_cast<int>(out) <= 126) {
+		PRINT_CHAR(static_cast<char>(out), "");
+	} else {
+		PRINT_CHAR("Not Displayable", "");
+	}
+	if (out > INT_MAX || out < INT_MIN) {
+		PRINT_INT("Impossible", "");
+	} else {
+		PRINT_INT(static_cast<int>(out), "");
+	}
+	if (out > std::numeric_limits<float>::max() || out < std::numeric_limits<float>::min()) {
+		PRINT_FLOAT("Impossible", "");
+	} else {
+		PRINT_FLOAT(static_cast<float>(out), "f");
+	}
+	PRINT_DOUBLE(out, "");
+}
+
+void	handle_float( const std::string str ) {
+	std::stringstream ss(str);
+	float out;
+	ss >> out;
+	if (ss.fail()) {
+		PRINT_CHAR("impossible", "");
+		PRINT_INT("impossible", "");
+		PRINT_FLOAT("impossible", "");
+		PRINT_DOUBLE("impossible", "");
+		return ;
+	}
+	if (isNaN(out)) {
+		PRINT_CHAR("impossible", "");
+		PRINT_INT("impossible", "");
+		PRINT_FLOAT(out, "f");
+		PRINT_DOUBLE(static_cast<double>(out), "");
+		return ;
+	}
+	if (isInf(out)) {
+		PRINT_CHAR("impossible", "");
+		PRINT_INT("impossible", "");
+		PRINT_FLOAT(out, "f");
+		PRINT_DOUBLE(static_cast<double>(out), "");
+		return ;
+	}
+	if (static_cast<int>(out) >= 32 && static_cast<int>(out) <= 126) {
+		PRINT_CHAR(static_cast<char>(out), "");
+	} else {
+		PRINT_CHAR("Not Displayable", "");
+	}
+	if (out > INT_MAX || out < INT_MIN) {
+		PRINT_INT("Impossible", "");
+	} else {
+		PRINT_INT(static_cast<int>(out), "");
+	}
+	PRINT_FLOAT(static_cast<float>(out), "f");
+	PRINT_DOUBLE(out, "");
+}
+
+void	handle_char( const std::string str ) {
+	std::stringstream ss(str);
+	char out;
+	ss >> out;
+	if (ss.fail()) {
+		PRINT_CHAR("impossible", "");
+		PRINT_INT("impossible (overflow)", "");
+		PRINT_FLOAT("impossible", "");
+		PRINT_DOUBLE("impossible", "");
+		return;
+	}
+	PRINT_CHAR(out, "");
+	PRINT_INT(static_cast<int>(out), "");
+	PRINT_FLOAT(static_cast<float>(out), ".0f");
+	PRINT_DOUBLE(static_cast<double>(out), ".0");
 }
 
 void	ScalarConverter::convert( const std::string &str ) {
@@ -64,6 +176,7 @@ void	ScalarConverter::convert( const std::string &str ) {
 			std::cerr << "Invalid Type" << std::endl;
 			return ;
 		}
+		std::cout << "Type: <Int>" << std::endl;
 		handle_int(str);
 		return ;
 	}
@@ -73,7 +186,8 @@ void	ScalarConverter::convert( const std::string &str ) {
 			std::cerr << "Invalid Type" << std::endl;
 			return ;
 		}
-		std::cout << "double " << std::endl;
+		std::cout << "Type: <Double> " << std::endl;
+		handle_double(str);
 		return ;
 	}
 	//handle float
@@ -82,12 +196,15 @@ void	ScalarConverter::convert( const std::string &str ) {
 			std::cerr << "Invalid Type" << std::endl;
 			return ;
 		}
-		std::cout << "Float" << std::endl;
+		std::string new_str = str.substr(0, str.length() - 1);
+		std::cout << "Type: <Float>" << std::endl;
+		handle_float(new_str);
 		return ;
 	}
 	//handle char
 	if (str.length() == 1 && str[0] >= 32 && str[0] <= 126) {
-		std::cout << "Char" << std::endl;
+		std::cout << "Type: <Char>" << std::endl;
+		handle_char(str);
 		return ;
 	}
 	std::cerr << "Invalid Type" << std::endl;
