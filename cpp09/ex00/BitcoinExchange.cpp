2e7b99b83ad5f205d94c1700152ee16d9f3c3e4f
Aymen Zainabi <azainabi@e1r13p2.1337.ma>
2025-05-02 03:55:03 +0100

    fixed some cpp09-ex00 issues

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -40,13 +40,13 @@ size_t BitcoinExchange::ConvertDate( std::string date ) const {
 	//y-m-d => y * 1000 + m * 100 + d
 	size_t pos = 0;
 	size_t result = 0;
-	int		factor = 1000;
+	int		factor = 10000;
 	while ((pos = date.find("-")) != std::string::npos) {
 		std::string element = date.substr(0, pos);
 		date.erase(0, pos + 1);
 		result += ConvertInt(element) * factor;
-		factor /= 10;
-		if (factor == 10) {
+		factor /= 100;
+		if (factor == 1) {
 			result += ConvertInt(date);
 		}
 	}
@@ -63,7 +63,18 @@ std::string BitcoinExchange::CleanLine(std::string& line) const {
 	return result;
 }
 
-int	BitcoinExchange::IsDateCorrect(std::string date) const {
+bool BitcoinExchange::IsLeapYear( int year ) const {
+	if (year % 400 == 0) {
+		return true;
+	} else if (year % 100 == 0) {
+		return false;
+	} else if (year % 4 == 0) {
+		return true;
+	}
+	return false;
+}
+
+int	BitcoinExchange::IsDateCorrect(std::string date)  {
 	size_t pos = 0;
 	size_t i = 0;
 	std::string date_arr[3];
@@ -75,6 +86,7 @@ int	BitcoinExchange::IsDateCorrect(std::string date) const {
 	}
 	date_arr[i] = date;
 	if (date_arr[0].empty() || date_arr[1].empty() || date_arr[2].empty()){
+		this->_error_log  = "Date is invalid, should be y-m-d";
 		return -1;
 	}
 
@@ -82,15 +94,33 @@ int	BitcoinExchange::IsDateCorrect(std::string date) const {
 	int month = ConvertInt(date_arr[1]);
 	int day = ConvertInt(date_arr[2]);
 
-	std::time_t t = std::time(NULL);
-	std::tm * time_info = std::localtime(&t);
-	int currentYear = 1900 + time_info->tm_year; 
+	if (year < 2009) {
+		this->_error_log = std::string("BitCoin is not created yet!");
+		return -1;
+	}
+	if (month <= 0 || month > 12) {
+		this->_error_log = "month should be between [1, 12]!";
+		return -1;
+	}
+	if (month == 2) {
+		if (day > 29) {
+			this->_error_log = "February does not have more that a maximum of 29 days!";
+			return -1;
+		}
+		if (!IsLeapYear(year)) {
 
-	if (year < 2009 || year > currentYear) return -1;
-	if (month <= 0 || month > 12) return -1;
-	if (day <= 0 || day > 31) return -1;
+			if (day > 28) {
+				this->_error_log = date_arr[0] + " is a not a leap year, thus February can only have 28 days!";
+				return -1;
+			}
+		}
+	}
+	if (day <= 0 || day > 31) {
+		this->_error_log = "day should be between [1, 31]!";
+		return -1;
+	}
 	
-	return year * 1000 + month * 100 + day;
+	return year * 10000 + month * 100 + day;
 }
 
 void BitcoinExchange::ParseInput() {
@@ -110,26 +140,33 @@ void BitcoinExchange::ParseInput() {
 		date = CleanLine(date);
 		line = CleanLine(line);
 
+		if (line.empty() || date.empty()) {
+			std::cout << "Error: No date or value is given" << std::endl;
+			continue;
+		}
+
 		if (date == "date" && line == "value") {
 			continue;
 		}
 
 		int key_date = IsDateCorrect(date);
 		if (key_date == -1) {
-			std::cout << "Error: '" << date << "' is incorrect" << std::endl;
+			std::cout << "Error: '" << date << "' is incorrect because: '" << this->_error_log << "'" << std::endl;
 			continue;
 		}
 		double value = ConvertDouble(line);
 		if (value < 0 || value > 1000) {
 			if (value < 0) {
-				std::cout << "Error: Value should be positive" << std::endl;
+				std::cout << "Error: ['"  <<  value <<"'] should be positive" << std::endl;
 			} else {
-				std::cout << "Error: Value Too Big" << std::endl;
+				std::cout << "Error: ['" << value  <<"'] Too Big" << std::endl;
 			}
 			continue;
 		}
-		std::map<size_t, double>::iterator it = this->_exchange_db.lower_bound(key_date);
-		if (it != _exchange_db.end()) {
+		std::map<size_t, double>::iterator it = this->_exchange_db.upper_bound(key_date);
+		if (it != _exchange_db.begin()) {
+			--it;
+			// std::cout << date << " => [" << it->first << "] " <<  value << " * " << it->second << " = " << value * it->second << std::endl;
 			std::cout << date << " => " << value << " = " << value * it->second << std::endl;
 		}
 	}
@@ -150,6 +187,7 @@ void BitcoinExchange::ParseDB() {
 		line.erase(0, line.find(",") + 1);
 		int date = ConvertDate(date_str);
 		double exchange_rate = ConvertDouble(line);
+		// std::cout << date << std::endl;
 		_exchange_db[date] = exchange_rate;
 	}
 	file.close();
